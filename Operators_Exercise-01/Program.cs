c8b699e97c270c6309e99662de961f6a70bf8bbc
Jacob Phillips <jakephillips.wv@gmail.com>
2022-09-14 13:57:47 -0500

    initial commit

--- Operators_Exercise-01/Program.cs
+++ Operators_Exercise-01/Program.cs
@@ -11,75 +11,103 @@ namespace Operators_Exercise_01
             int x = 10;
             int y = 20;
 
-            // TODO: Add 20 to x and 60 to y, print them out to the console, then AddSpace()
+            // DONE: Add 20 to x and 60 to y, print them out to the console, then AddSpace()
             x += 20;
             y += 60;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: x = 30
+            // DONE: y = 80
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
-            // TODO: Subtract 10 from x and 70 from y, print them out to the console, then AddSpace()
+            // DONE: Subtract 10 from x and 70 from y, print them out to the console, then AddSpace()
             x -= 10;
             y -= 70;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: x = 20
+            // DONE: y = 10
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
-            // TODO: Multiply x by 3 and y by 2, print them out to the console, then AddSpace()
+            // DONE: Multiply x by 3 and y by 2, print them out to the console, then AddSpace()
             x *= 3;
             y *= 2;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: x = 60
+            // DONE: y = 20
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
             // TODO: Divide x by 6 and y by 1, print them out to the console, then AddSpace()
             x /= 6;
             y /= 1;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: x = 10
+            // DONE: y = 20
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
-            // TODO: Modulus x by 3 and y by 3, print them out to the console, then AddSpace()
-            x %= 2;
-            y %= 2;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: Modulus x by 3 and y by 3, print them out to the console, then AddSpace()
+            x %= 3;
+            y %= 3;
+            // DONE: x = 1
+            // DONE: y = 2
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
-            // TODO: Pre-increment x and y, print them out to the console, then AddSpace()
+            // DONE: Pre-increment x and y, print them out to the console, then AddSpace()
             ++x;
             y++;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: x = 2
+            // DONE: y = 3
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
             // Thought Experiment: What will the value of x and y be after going through this pre/post increment operation?
             x = ++x;
             y = y++;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: x = 3
+            // DONE: y = 3
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
             // Thought Experiment: What will the value of x and y be after going thought the pre/post increment again, only slightly changed up a bit?
             x++;
             ++y;
-            // TODO: x = ?
-            // TODO: y = ?
+            // DONE: x = 4
+            // DONE: y = 4
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
 
             AddSpace();
 
             // Thought Experiment: What will be the value of x, y, and z that is written to the console?
             int z = --y + ++x;
-            // TODO: x = ?
-            // TODO: y = ?
-            // TODO: z = ?
+            // DONE: x = 5
+            // DONE: y = 3
+            // DONE: z = 8
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
 
             AddSpace();
 
@@ -95,62 +123,67 @@ namespace Operators_Exercise_01
             bool second = true;
 
             // Thought Experiment: What is the value of the variable conditionalValue after the == operator?
-            conditionalValue = first == second;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = first == second);
+            // DONE: conditionalValue = True
 
             AddSpace();
 
             // Thought Experiment: What is the value of the variable conditionalValue after the != operator?
-            conditionalValue = first != second;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = first != second);
+            // DONE: conditionalValue = False
 
             AddSpace();
 
             // Thought Experiment: What is the value of the variable conditionalValue after the > operator?
-            conditionalValue = a > b;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = a > b);
+            // DONE: conditionalValue = False
 
             AddSpace();
 
             // Thought Experiment: What is the value of the variable conditionalValue after the < operator?
-            conditionalValue = a < b;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = a < b);
+            // DONE: conditionalValue = True
 
             AddSpace();
 
             // Thought Experiment: What is the value of the variable conditionalValue after the >= operator?
-            conditionalValue = a >= b;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = a >= b);
+            // DONE: conditionalValue = False
 
             AddSpace();
 
             // Thought Experiment: What is the value of the variable conditionalValue after the <= operator?
-            conditionalValue = a <= b;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = a <= b);
+            // DONE: conditionalValue = True
 
             AddSpace();
 
             #endregion
 
             #region/*===================================================   Logical Operators   ======================================================*/
+            //From above:
+            // int a = 10;
+            // int b = 11;
+            // bool first = true;
+            // bool second = true;
 
             // Thought Experiement: What is the value of the variable conditionalValue with the logical operator
-            conditionalValue = a < b && ++a == b;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = a < b && ++a == b);
+            // DONE: conditionalValue = True
 
             AddSpace();
 
             // Thought Experiement: What is the value of the variable conditionalValue with the logical operator
-            conditionalValue = a++ == b || first == second;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = a++ == b || first == second);
+            // DONE: conditionalValue = True
 
             AddSpace();
 
             first = false;
             second = true;
             // Thought Experiement: What is the value of the variable conditionalValue with the logical operator
-            conditionalValue = !second == first && a >= --b;
-            // TODO: conditionalValue = ?
+            Console.WriteLine(conditionalValue = !second == first && a >= --b);
+            // DONE: conditionalValue = True
 
             AddSpace();
 
@@ -162,39 +195,39 @@ namespace Operators_Exercise_01
             double var1 = 10.50; // Simple assignment operator =
             double var2 = 3.25; // Simple assignment operator =
 
-            // TODO: use the Simple Assignment operator to add var1 and var2 and then assign the sum to the total variable
-            total = var1 + var2;
-            // TODO: total = ?
+            // DONE: use the Simple Assignment operator to add var1 and var2 and then assign the sum to the total variable
+            Console.WriteLine(total = var1 + var2);
+            // DONE: total = 13.75
 
             AddSpace();
 
             // Thought Experiment: What's the result doing the same operation, but replacing the Simple Assignment operator with the Add Assignment operator?
-            total += var1 + var2;
-            // TODO: total = ?
+            Console.WriteLine(total += var1 + var2);
+            // DONE: total = 27.5
 
             AddSpace();
 
-            // TODO: use the Subtract Assignment operator to subtract total from var1 
-            total -= var1;
-            // TODO: total = ?
+            // DONE: use the Subtract Assignment operator to subtract total from var1 
+            Console.WriteLine(total -= var1);
+            // DONE: total = 17
 
             AddSpace();
 
-            // TODO: use the Multiply Assignment operator to multiply total by var2
-            total *= var2;
-            // TODO: total = ?
+            // DONE: use the Multiply Assignment operator to multiply total by var2
+            Console.WriteLine(total *= var2);
+            // DONE: total = 55.25
 
             AddSpace();
 
-            // TODO: use the Division Assignment operator to divide total by 5
-            total /= 5;
-            // TODO: total = ?
+            // DONE: use the Division Assignment operator to divide total by 5
+            Console.WriteLine(total /= 5);
+            // DONE: total = 11.05
 
             AddSpace();
 
-            // TODO: use the Modulus Assignment operator to modulo the total by 11.05
-            total %= 11.05;
-            // TODO: total = ?
+            // DONE: use the Modulus Assignment operator to modulo the total by 11.05
+            Console.WriteLine(total %= 11.05);
+            // DONE: total = 0
 
             AddSpace();
 
@@ -208,7 +241,7 @@ namespace Operators_Exercise_01
             int int1 = 1;
             int int2 = 2;
 
-            // TODO: use the Ternary operator to shorten this if/else statement with syntax sugar
+            // DONE: use the Ternary operator to shorten this if/else statement with syntax sugar
             /*
             if (finalString.Length == 0)
             {
@@ -219,12 +252,12 @@ namespace Operators_Exercise_01
                 finalString = str2;
             }
             */
-            finalString = finalString.Length == 0 ? str1 : str2;
-            // TODO: finalString = ?
+            Console.WriteLine(finalString = finalString.Length == 0 ? str1 : str2);
+            // DONE: finalString = String 1
 
             AddSpace();
 
-            // TODO: use the Ternary operator to shorten this if statement with syntax sugar
+            // DONE: use the Ternary operator to shorten this if statement with syntax sugar
             /*
             if (int1 + int2 > intTotal)
             {
@@ -235,14 +268,14 @@ namespace Operators_Exercise_01
                 intTotal = int1 - int2;
             }
             */
-            intTotal = int1 + int2 > intTotal ? int1 + int2 : int1 - int2;
-            // TODO: intTotal = ?
+            Console.WriteLine(intTotal = int1 + int2 > intTotal ? int1 + int2 : int1 - int2);
+            // DONE: intTotal = 3
 
             AddSpace();
 
             // Thought Experiment: What will be the value of finalString after the Ternary operation?
-            finalString = finalString != null && finalString.Length != 0 ? str1[7].ToString() : str2[7].ToString();
-            // TODO: finalString = ?
+            Console.WriteLine(finalString = finalString != null && finalString.Length != 0 ? str1[7].ToString() : str2[7].ToString());
+            // DONE: finalString = 1
 
             #endregion
         }
